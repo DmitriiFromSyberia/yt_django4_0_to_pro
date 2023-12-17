861880bc1d08f402e3850dbbcd05d5b5c2ca18f1
Быков Дмитрий <prepodamurok@gmail.com>
2023-12-17 16:46:30 -0500

    form add and update delete

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
-from myapp.views import index, indexItem, add_item, update_item
+from myapp.views import index, indexItem, add_item, update_item, delete_item
 
 app_name = "myapp"
 
 urlpatterns = [
     # http://127.0.0.1:8000/myapp/hello/
-    path("", index),
+    path("", index, name="index"),
     path("<int:my_id>/", indexItem, name="detail"),
     # http://127.0.0.1:8000/myapp/contacts/
     # path('contacts/', contacts)
     path("additem/", add_item, name="add_item"),
     path("updateitem/<int:my_id>/", update_item, name="update_item"),
+    path("deleteitem/<int:my_id>/", delete_item, name="delete_item"),
 ]
