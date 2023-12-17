ba803828a3dee53d03b85e374a635de427c1f909
Быков Дмитрий <prepodamurok@gmail.com>
2023-12-17 16:12:53 -0500

    form add and update

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Product
 
@@ -16,6 +16,30 @@ def indexItem(request, my_id):
     # return HttpResponse('Your item id is:'+ str(my_id))
     context = {"item": item}
     return render(request, "myapp/detail.html", context)
+
+
+def add_item(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        description = request.POST.get("description")
+        image = request.FILES["upload"]
+        item = Product(name=name, price=price, description=description, image=image)
+        item.save()
+    return render(request, "myapp/additem.html")
+
+
+def update_item(request, my_id):
+    item = Product.objects.get(id=my_id)
+    if request.method == "POST":
+        item.name = request.POST.get("name")
+        item.price = request.POST.get("price")
+        item.description = request.POST.get("description")
+        item.image = request.FILES.get('upload', item.image)
+        item.save()
+        return redirect("/myapp/")
+    context = {"item": item}
+    return render(request, "myapp/updateitem.html", context)
 
 
 # def contacts (request):
