25d437fe7977bd481fa318a018636395005d72df
Быков Дмитрий <prepodamurok@gmail.com>
2023-12-27 22:03:27 -0500

    users add2

--- mysite/users/forms.py
+++ mysite/users/forms.py
@@ -1,3 +1,4 @@
+# from typing import Any
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
@@ -35,3 +36,10 @@ class NewUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+    # def save(self, commit=True):
+    #     user = super(NewUserForm, self).save(commit=False)
+    #     user.email=self.cleaned_data["email"]
+    #     if commit:
+    #         user.save()
+    #         return user
